3579c834ff2c3cdaa20e6c6cd298dbf538d0183e
Johnny <jonathanbc2012@gmail.com>
2024-08-20 18:30:09 +0200

    function llenarSelect added to app.js and readme updated

--- 17-PROYECTO-Buscador/README.md
+++ 17-PROYECTO-Buscador/README.md
@@ -37,4 +37,37 @@ function mostrarVehiculos() {
 }
 ```
 
+## Segundo paso: crear un select dinámico que muestre los últimos 20 años.
 
+Queremos que se generen de forma dinámica los últimos 20 años en el campo año y aparezca cada uno de los años como opción de selección para el usuario. Para ello, declararemos 3 variables, una con el selector y otras dos con el rango de años que queremos, apoyado en una de las funciones para manejar fechas que veremos próximamente:
+
+```javascript
+const year = document.querySelector("#year");
+const max = new Date().getFullYear(); // Devuelve el año actual
+const min = max - 20; // resta 20 al año actual
+```
+
+Actualizamos el evento para que se carguen las opciones al inicio:
+
+```javascript
+// Eventos
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarVehiculos();
+
+  // Rellenar el input con las opciones de años
+  llenarSelect();
+});
+```
+
+Creamos la función **_llenarSelect()_**:
+
+```javascript
+function llenarSelect() {
+  for (let i = max; i >= min; i--) {
+    const opcion = document.createElement("OPTION");
+    opcion.value = i;
+    opcion.textContent = i;
+    year.appendChild(opcion);
+  }
+}
+```
--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,15 +1,22 @@
 //Variables
-const resultado = document.querySelector('#resultado')
+const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear(); // Devuelve el año actual
+const min = max - 20; // resta 20 al año actual
 
 // Eventos
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     mostrarVehiculos();
+
+    // Rellenar el input con las opciones de años
+    llenarSelect();
 })
 
 // Funciones
 function mostrarVehiculos() {
-    vehiculos.forEach(vehiculo =>{
-        const {marca, modelo, year, puertas, transmision, precio, color} = vehiculo;
+    vehiculos.forEach(vehiculo => {
+        const { marca, modelo, year, puertas, transmision, precio, color } = vehiculo;
 
         const vehiculoHTML = document.createElement('P');
 
@@ -21,3 +28,14 @@ function mostrarVehiculos() {
         resultado.appendChild(vehiculoHTML)
     })
 };
+
+// Generar los últimos 20 años en el campo año
+function llenarSelect() {
+    for (let i = max; i >= min; i--) {
+        const opcion = document.createElement('OPTION');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion)
+    }
+}
+
